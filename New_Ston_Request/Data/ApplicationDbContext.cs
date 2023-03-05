9c8f5a8466b544319152fe94458bfce1b104923e
MujtabaAbdullahi <mujtabamuhammadi037@gmail.com>
2023-03-05 09:19:54 +0430

    Database connection

---
+++ New_Ston_Request/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using New_Ston_Request.Models;
+
+namespace New_Ston_Request.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
---
+++ New_Ston_Request/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace New_Ston_Request.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        
+
+    }
+}
