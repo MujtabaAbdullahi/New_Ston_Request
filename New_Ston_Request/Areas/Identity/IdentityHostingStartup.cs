9e51bdd1b297655517f6646b66764ac0c83f4c5f
MujtabaAbdullahi <mujtabamuhammadi037@gmail.com>
2023-03-17 10:17:21 +0430

    authontication

---
+++ New_Ston_Request/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using New_Ston_Request.Data;
+
+[assembly: HostingStartup(typeof(New_Stone_Request.Areas.Identity.IdentityHostingStartup))]
+namespace New_Stone_Request.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
